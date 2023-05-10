1f778aea2eacddef908392240d93b0ab6e467732
croat25 <sala4460@mylaurier.ca>
2023-05-10 11:02:50 -0400

    inital setup of website

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
+
+
+class CampaignIndex extends Component {
+    static async getInitialProps(){
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        
+        return { campaigns };
+    }
+
+    render () {
+        return <div>Address {this.props.campaigns[0]}</div>
+    }
+}
+
+export default CampaignIndex;
